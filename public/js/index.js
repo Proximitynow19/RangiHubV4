7754dcf5f7a4c6b79beaa28d15555348030b4a72
Proximitynow19 <142328@cloud.rangitoto.school.nz>
2021-11-23 01:09:46 +1300

    navbar thing

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,10 @@ window.addEventListener("DOMContentLoaded", () => {
   const blurhash = document.querySelector("blurhash-img");
   blurhash.setAttribute(
     "style",
-    `--aspect-ratio: ${$("header").height() / $(document).width()};`
+    `--aspect-ratio: ${
+      document.getElementsByTagName("header")[0].offsetHeight /
+      document.getElementsByTagName("header")[0].offsetWidth
+    };`
   );
 });
 
@@ -12,4 +15,18 @@ window.addEventListener("load", () => {
   setTimeout(() => {
     blurhash.parentElement.removeChild(blurhash);
   }, 1000);
+});
+
+window.addEventListener("scroll", () => {
+  const dynamicNav = document.getElementById("dynamic-nav");
+
+  if (
+    window.scrollY > document.getElementsByTagName("header")[0].offsetHeight
+  ) {
+    if (dynamicNav.classList.contains("hidden"))
+      dynamicNav.classList.remove("hidden");
+  } else {
+    if (!dynamicNav.classList.contains("hidden"))
+      dynamicNav.classList.add("hidden");
+  }
 });
